92539f030e96cad247b629a491644b982903bd62
mao.buidinh <mao.buidinh@hotmail.com>
2018-07-23 21:57:31 +0700

    - init prj.

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
 # espresso
+
+https://classroom.udacity.com/courses/ud855/lessons/f0084cc7-2cbc-4b8e-8644-375e8c927167/concepts/d37b0c0b-0fbc-4fe6-9d69-d5f1415b318d
+
---
+++ app/src/main/java/com/me/bui/teatime/model/Tea.java
@@ -0,0 +1,28 @@
+package com.me.bui.teatime.model;
+
+
+/**
+ * {@link Tea} represents a tea that the user can select from the menu.
+ * It contains a tea name and an associated.
+ */
+
+public class Tea {
+
+    private String mTeaName;
+    private int mImageResourceId;
+
+    public Tea(String teaName, int imageResourceId) {
+        mTeaName = teaName;
+        mImageResourceId = imageResourceId;
+    }
+
+    public String getTeaName() {
+        return mTeaName;
+    }
+
+    public int getImageResourceId() {
+        return mImageResourceId;
+    }
+
+}
+
